48acc3d6d9ed423c70ab7d334dd95b800a3f5971
Myron <dubtwee@gmail.com>
2020-03-26 16:26:04 -0500

    creating a inventory route to return all inventory

--- server/server.js
+++ server/server.js
@@ -4,8 +4,23 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const inventory = [
+  {
+    name: 'pineapple',
+    description: 'sweet and delicious',
+  },
+  {
+    name: 'skittles',
+    description: 'why did they change green?',
+  },
+];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get('/inventory', (req, res) => {
+  res.send(inventory);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
