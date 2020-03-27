a7a32ad266f855053423a1a8832cdce934be5f48
Myron <dubtwee@gmail.com>
2020-03-26 22:41:43 -0500

    loading the inventory and rendering it to the page when the page has loaded

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,36 @@ $(document).ready(init);
 
 function init() {
   console.log('DOM is READY!!!');
+
+  getInventory();
+}
+
+function getInventory() {
+  // AJAX request to server for inventory
+  $.ajax({
+    method: 'GET',
+    url: '/inventory',
+  })
+  .then((response) => {
+    render(response);
+  })
+  .catch((err) => {
+    console.log('err');
+    alert('There was an error finding inventory.');
+  });
+}
+
+
+// render inventory to the DOM
+function render(inventoryList) {
+  console.log('RENDER INVENTORY');
+  $('.js-inventory-list').empty();
+
+  for (let i = 0; i < inventoryList.length; i++) {
+    const inventoryItem = inventoryList[i];
+
+    $('.js-inventory-list').append(`
+      <p><strong>${inventoryItem.name}:</strong> ${inventoryItem.description}</p>
+    `);
+  }
 }
